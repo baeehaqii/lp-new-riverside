9a9f6336db69ce05abf9e319420c359dc1cf9f65
Unname <muhamadimambaehaqi@gmail.com>
2026-01-26 10:53:20 +0700

    update gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,11 +63,22 @@ export default function RootLayout({
         `}} />
 
         {/* Google Tag Manager */}
-
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PB3J38GD');`,
+          }}
+        />
         {/* End Google Tag Manager */}
 
         {/* Facebook Pixel Code */}
-        <Script
+        {/* COMMENTED OUT - Using Google Tag Manager instead */}
+        {/* <Script
           id="facebook-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -84,11 +95,12 @@ export default function RootLayout({
               fbq('track', 'PageView');
             `,
           }}
-        />
+        /> */}
         {/* End Facebook Pixel Code */}
 
         {/* Facebook Pixel (noscript) */}
-        <noscript>
+        {/* COMMENTED OUT - Using Google Tag Manager instead */}
+        {/* <noscript>
           <img
             height="1"
             width="1"
@@ -96,7 +108,7 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=480399854494345&ev=PageView&noscript=1"
             alt=""
           />
-        </noscript>
+        </noscript> */}
         {/* End Facebook Pixel (noscript) */}
 
         {/* TikTok Pixel Code */}
@@ -105,7 +117,14 @@ export default function RootLayout({
       </head>
       <body style={{ fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif' }}>
         {/* Google Tag Manager (noscript) */}
-
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PB3J38GD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
 
         <Suspense fallback={null}>
